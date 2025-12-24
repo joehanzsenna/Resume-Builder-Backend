00b147cb40d3f49548736892286d750bb0c5056e
joehanzsenna <jonathanedison34@gmail.com>
2025-12-24 18:03:56 +0100

    updated server to use cors simply without credentials

--- server.js
+++ server.js
@@ -11,22 +11,22 @@ const PORT = process.env.PORT || 3001;
 
 const allowedOrigins = ['http://localhost:5173'];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // allow requests with no origin (like curl, Postman)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'), false);
-    }
-  },
-  credentials: true,
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization','Accept'],
-  preflightContinue: false, // let cors finish OPTIONS response
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // allow requests with no origin (like curl, Postman)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) !== -1) {
+//       return callback(null, true);
+//     } else {
+//       return callback(new Error('Not allowed by CORS'), false);
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+//   allowedHeaders: ['Content-Type','Authorization','Accept'],
+//   preflightContinue: false, // let cors finish OPTIONS response
+//   optionsSuccessStatus: 204,
+// };
 // app.use(cors({
 //   origin: "http://localhost:5173",
 //   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -34,7 +34,8 @@ const corsOptions = {
 // }));
 
 // register CORS before routes
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors())
 
 app.use(express.json());
 
